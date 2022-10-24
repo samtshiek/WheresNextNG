3699f28de6e22769bef6de351e0fefaf071a3e90
ssteph-sudo <77129616+ssteph-sudo@users.noreply.github.com>
2022-10-24 16:04:14 -0700

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProfileComponent } from './profile/profile.component';
-import { FavoritesComponent } from './favorites/favorites.component';
-import { FeedbackComponent } from './feedback/feedback.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTabsModule} from '@angular/material/tabs';
+
+import { SignInComponent}  from './login/login.component';  
+import { GoogleSigninComponent }  from './google.signin';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    ProfileComponent,
-    FavoritesComponent,
-    FeedbackComponent
+    GoogleSigninComponent,
+    SignInComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatTabsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
