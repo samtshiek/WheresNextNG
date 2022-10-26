d848b6d67576943bbdd9e7c23f030199d44bdb35
ssteph-sudo <77129616+ssteph-sudo@users.noreply.github.com>
2022-10-25 19:18:42 -0700

    Merged all the components everyone did and added menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,37 @@ export class AppComponent {
   title = 'WheresNext';
   loggedIn: boolean = false;
   currentUser: string = 'Please Log In';
-
-
-  ngOnInit(): void {
-    if(sessionStorage.getItem('ID:') === null){
-      this.loggedIn = false;
-    }
-      else if(sessionStorage.getItem('ID:').length > 5){
-      this.loggedIn = true;
-      this.currentUser = 'Welcome ' + sessionStorage.getItem('Name:');
-    }
-  }
+  navLinks: any[];
+  activeLinkIndex = -1;
  
-    
+  constructor(private router: Router) {
+    this.navLinks = [
+        {
+            label: 'Home',
+            link: './',
+            index: 0
+        }, {
+            label: 'Profile',
+            link: './Profile',
+            index: 1
+        }, {
+            label: 'Favorites',
+            link: './Favorites',
+            index: 2
+        }, 
+    ];
+}
+ngOnInit(): void {
+  this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+  });
+  if(sessionStorage.getItem('ID:') === null){
+    this.loggedIn = false;
+  }
+    else if(sessionStorage.getItem('ID:').length > 5){
+    this.loggedIn = true;
+    this.currentUser = 'Welcome ' + sessionStorage.getItem('Name:');
+  }
+}
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UsersComponent } from './users/users.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProfileComponent } from './profile/profile.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
 import { SignInComponent}  from './login/login.component';  
 import { GoogleSigninComponent }  from './google.signin';
 import { LoginComponent } from './login/login.component';
@@ -18,15 +27,25 @@ import { LogoutComponent } from './logout/logout.component';
     GoogleSigninComponent,
     SignInComponent,
     LoginComponent,
-    LogoutComponent
+    LogoutComponent,
+    UsersComponent,
+    ProfileComponent,
+    FavoritesComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
