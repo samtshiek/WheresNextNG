d087d8e33c77924120ee38896264305ac76fa641
bcuser <samtshiek@yahoo.fr>
2022-12-04 19:31:55 -0800

    setting up https urls for new ng wep app

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -19,24 +19,31 @@ export class UserService {
   ) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>('https://wheresnextnode.azurewebsites.net/users');
+    //https://wheresnextws.azurewebsites.net/
+    //return this.http.get<User[]>('https://wheresnextnode.azurewebsites.net/users');
+    return this.http.get<User[]>('https://wheresnextws.azurewebsites.net/users');
+  
   }
   getUser(id: string): Observable<User> {
-    return this.http.get<User>('https://wheresnextnode.azurewebsites.net/users/'+id);
+    //return this.http.get<User>('https://wheresnextnode.azurewebsites.net/users/'+id);
+    return this.http.get<User>('https://wheresnextws.azurewebsites.net/users/'+id);
   }
 
   loginUser(newUser: loginUser): Observable<string> {  // usin a post as I want to pass the data in the body
     console.log("We are gonna go to this url"+'https://wheresnextnode.azurewebsites.net/login');
     //return this.http.post<string>('https://wheresnextnode.azurewebsites.net/login', newUser);
-    return this.http.post<string>('http://localhost:3000/login', newUser);
+    //return this.http.post<string>('http://localhost:3000/login', newUser);
+    return this.http.post<string>('https://wheresnextws.azurewebsites.net/login', newUser);
   }
 
   newUser(newUser: NewUser): Observable<User> {
-    return this.http.post<User>('https://wheresnextnode.azurewebsites.net/users/create', newUser);
+    //return this.http.post<User>('https://wheresnextnode.azurewebsites.net/users/create', newUser);
+    return this.http.post<User>('https://wheresnextws.azurewebsites.net/users/create', newUser);
   }
   
   updateUser(user:User): Observable<User> {
-    return this.http.post<User>('https://wheresnextnode.azurewebsites.net/users/update_user/',user);
+    //return this.http.post<User>('https://wheresnextnode.azurewebsites.net/users/update_user/',user);
+    return this.http.post<User>('https://wheresnextws.azurewebsites.net/users/update_user/',user);
   }
 
   getLongLat(address: string): Observable<any> {
@@ -48,15 +55,17 @@ export class UserService {
 
   getPlacesNode(searchObject: any): Observable<any> {
     //let observable = this.http.post<any>('https://wheresnextnode.azurewebsites.net/users/get-places', searchObject);
-    let observable = this.http.post<any>('http://localhost:3000/users/get-places', searchObject);
+    //let observable = this.http.post<any>('http://localhost:3000/users/get-places', searchObject);
+    let observable = this.http.post<any>('https://wheresnextws.azurewebsites.net/users/get-places', searchObject);
     console.log("Node place result: ", observable);
 
     return observable;
   }
 
   getPlace(placeId: string): Observable<any> {
-    console.log("going to:'https://wheresnextnode.azurewebsites.net/users/get-place/id/"+placeId);
-    return this.http.get<any>('https://wheresnextnode.azurewebsites.net/users/get-place/id/'+placeId);
+    //console.log("going to:'https://wheresnextnode.azurewebsites.net/users/get-place/id/"+placeId);
+    //return this.http.get<any>('https://wheresnextnode.azurewebsites.net/users/get-place/id/'+placeId);
+    return this.http.get<any>('https://wheresnextws.azurewebsites.net/users/get-place/id/'+placeId);
   //  console.log("Node place result: ", observable);
 
    // return observable;
@@ -66,7 +75,9 @@ export class UserService {
 
     console.log("ParamObject type in method: ", ParamObject);
     //let observable = this.http.post<any>('https://wheresnextnode.azurewebsites.net/users/get-places-advanced', ParamObject);
-    let observable = this.http.post<any>('http://localhost:3000/users/get-places-advanced', ParamObject);
+    //let observable = this.http.post<any>('http://localhost:3000/users/get-places-advanced', ParamObject);
+    let observable = this.http.post<any>('https://wheresnextws.azurewebsites.net/users/get-places-advanced', ParamObject);
+   
     return observable;
   }
 
@@ -94,7 +105,8 @@ export class UserService {
 
   postResults(ansObject: any): Observable<any> {
     console.log("test user service", ansObject);
-    return this.http.post<any>('https://wheresnextnode.azurewebsites.net/users/submit-quiz', ansObject);
+    //return this.http.post<any>('https://wheresnextnode.azurewebsites.net/users/submit-quiz', ansObject);
+    return this.http.post<any>('https://wheresnextws.azurewebsites.net/users/submit-quiz', ansObject);
   }
 
   setGeo(geoObject: any) {
@@ -114,14 +126,17 @@ export class UserService {
 //     return this.http.post<any>('https://wheresnextnode.azurewebsites.net/users/add-favorite', {uid: uid, place: place});
 //   }
  addPlaceToFavorite(favObject: any): Observable<any> {
-    return this.http.post<any>('https://wheresnextnode.azurewebsites.net/users/add-place-to-favorite', favObject);
+    //return this.http.post<any>('https://wheresnextnode.azurewebsites.net/users/add-place-to-favorite', favObject);
+    return this.http.post<any>('https://wheresnextws.azurewebsites.net/users/add-place-to-favorite', favObject);
   }
   
   getFavoritePlace(place: any ): Observable<any> {
-    return this.http.get<any>('https://wheresnextnode.azurewebsites.net/users/get-favorite-place'+ place);
+    //return this.http.get<any>('https://wheresnextnode.azurewebsites.net/users/get-favorite-place'+ place);
+    return this.http.get<any>('https://wheresnextws.azurewebsites.net/users/get-favorite-place'+ place);
   }
   removeFavoritePlace(RemfavObject: any ): Observable<any> {
-   return this.http.post<any>('https://wheresnextnode.azurewebsites.net/users/remove-favorite', RemfavObject);
+   //return this.http.post<any>('https://wheresnextnode.azurewebsites.net/users/remove-favorite', RemfavObject);
+   return this.http.post<any>('https://wheresnextws.azurewebsites.net/users/remove-favorite', RemfavObject);
   }
  
 }
